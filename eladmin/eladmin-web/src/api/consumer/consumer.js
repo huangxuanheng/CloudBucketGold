bda581c537111c72f7344e2ed12704db8e66073a
huangxuanheng <huangxuanheng@163.com>
2020-07-20 21:11:56 +0800

    消費商

---
+++ eladmin/eladmin-web/src/api/consumer/consumer.js
@@ -0,0 +1,48 @@
+import request from '@/utils/request'
+import qs from 'qs'
+
+export function getConsumers(params) {
+  return request({
+    url: 'api/consumer/list',
+    method: 'get',
+    params
+  })
+}
+
+export function getConsumer(id) {
+    const params = {
+        id: id
+      }
+    return request({
+      url: 'api/consumer?'+qs.stringify(params, { indices: false }),
+      method: 'get'
+    })
+  }
+
+
+export function add(data) {
+  // debugger
+  return request({
+    url: 'api/consumer',
+    method: 'post',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: 'api/consumer',
+    method: 'delete',
+    data: ids
+  })
+}
+
+export function edit(data) {
+  return request({
+    url: 'api/consumer',
+    method: 'put',
+    data
+  })
+}
+
+export default { add, edit, del, getConsumers, getConsumer }
